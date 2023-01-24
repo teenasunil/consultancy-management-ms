a1db4805c549fe33a7bfd3d71e61a34839a592ef
teenasunil <teenamscs2016@gmail.com>
2023-01-24 16:16:02 -0500

    Creating the code

---
+++ consultancy-management-ms/src/main/java/com/micro/consultancymanagementms/ConsultancyManagementMsApplication.java
@@ -0,0 +1,13 @@
+package com.micro.consultancymanagementms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultancyManagementMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultancyManagementMsApplication.class, args);
+	}
+
+}
